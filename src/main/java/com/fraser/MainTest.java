d7a4fdf23eb153361198ebf7890b0ad9100de1f6
Chad Fraser <chad.fraser@mail.mcgill.ca>
2019-07-22 12:09:06 -0600

    Initial commit

---
+++ src/main/java/com/fraser/MainTest.java
@@ -0,0 +1,41 @@
+package com.fraser;
+
+import java.security.Key;
+import java.util.Arrays;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.KeyType;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
+
+public class MainTest {
+    public static void main(String[] args) {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000",
+                        "us-west-2"))
+                .build();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+        String tableName = "movies";
+        try {
+            System.out.println("Attempting to create table, please wait...");
+            Table table = dynamoDB.createTable(tableName,
+                    Arrays.asList(new KeySchemaElement("year", KeyType.HASH),
+                            new KeySchemaElement("title", KeyType.RANGE)),
+                    Arrays.asList(new AttributeDefinition("year", ScalarAttributeType.N),
+                            new AttributeDefinition("title", ScalarAttributeType.S)),
+                    new ProvisionedThroughput(10L, 10L));
+            table.waitForActive();
+            System.out.println(String.format("Success! Table status: %s", table.getDescription()));
+        } catch (Exception e) {
+            System.err.println("Unable to create table.");
+            System.err.println(e.getMessage());
+        }
+    }
+}
